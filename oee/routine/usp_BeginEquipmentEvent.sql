0ee3706954785888efdd6dc53683edbda4443c74
Tony Upton <13627984+uptona@users.noreply.github.com>
2024-02-09 22:42:48 -0500

    Refactor SQL scripts for readability and accuracy
    Enhanced the 'test.sql' script's precision by amending the equipment path for better data mapping. The stored procedure 'usp_BeginEquipmentEvent.sql' underwent significant refactoring as part of this commit to enhance code readability: the assignment of the current UTC datetime if input is null is now clearly described, and the 'INSERT INTO' statement logic was revised to insert values directly. This makes the functionality more straightforward to comprehend and maintain in future.

--- oee/routine/usp_BeginEquipmentEvent.sql
+++ oee/routine/usp_BeginEquipmentEvent.sql
@@ -1,4 +1,3 @@
-
 CREATE PROCEDURE [OEE].[usp_BeginEquipmentEvent] (
 	@equipmentId int,
 	@beginTime datetime = NULL,
@@ -7,41 +6,67 @@ CREATE PROCEDURE [OEE].[usp_BeginEquipmentEvent] (
 AS
 BEGIN
 
-    -- Set @beginTime to current time if NULL
+    -- Assign the current UTC date time to @beginTime if it's NULL
 	IF @beginTime IS NULL SET @beginTime = SYSUTCDATETIME()
 
-    DECLARE @lastEventId int = OEE.fn_FindLastEquipmentEvent(@equipmentId, @beginTime)
+    -- Declare various variables to store the relevant data from the last recorded equipment event
+    DECLARE
+        @equipmentEventId int,
+        @stateEventId int,
+        @jobEventId int,
+        @shiftEventId int,
+        @performanceEventId int,
+        @lastBeginTime datetime,
+        @lastEndTime datetime;
 
-    DECLARE @lastEventTime datetime = (SELECT BeginTime FROM OEE.EquipmentEvents WHERE Id = @lastEventId)
+    -- Query the last event for the specified equipment from the EquipmentEvents table
+    SELECT TOP (1)
+        @equipmentEventId = EE.Id,                -- Event's unique identifier
+        @stateEventId = EE.StateEventId,          -- State event associated with the equipment event
+        @jobEventId = EE.JobEventId,              -- Job event associated with the equipment event
+        @shiftEventId = EE.ShiftEventId,          -- Shift event associated with the equipment event
+        @performanceEventId = EE.PerformanceEventId, -- Performance event associated with the equipment event
+        @lastBeginTime = EE.BeginTime,                -- Start time of the event
+        @lastEndTime = EE.EndTime                     -- End time of the event
+    FROM OEE.EquipmentEvents EE
+    WHERE EquipmentId = @equipmentId            -- Filter by provided equipmentId
+    AND BeginTime <= @beginTime                     -- Ensure to pick an event that started before or at the specified time
+    ORDER BY BeginTime DESC;                        -- Sort events based on BeginTime, most recent first
 
-    IF @lastEventTime > @beginTime RETURN
-
-    IF @lastEventTime = @beginTime
+    -- If beginTime matches with a recorded event's begin time, assign the matching event's ID to @eventId and end the procedure
+    IF @lastBeginTime  = @beginTime
     BEGIN
-        SET @eventId = @lastEventId
+        SET @eventId = @equipmentEventId
         RETURN
     END
 
-    IF @lastEventId IS NOT NULL
+    -- If the equipment event ID is not NULL, update the EndTime of the event to @beginTime in the EquipmentEvents table
+    IF @equipmentEventId IS NOT NULL
     BEGIN
         UPDATE OEE.EquipmentEvents
         SET EndTime = @beginTime
-        WHERE Id = @lastEventId
+        WHERE Id = @equipmentEventId
     END
 
-    INSERT INTO OEE.EquipmentEvents
-    (EquipmentId, StateEventId, JobEventId, ShiftEventId, PerformanceEventId, BeginTime)
-    SELECT TOP (1)
-        @equipmentId,
+    -- Insert a new record in the EquipmentEvents table with the provided and fetched details and assign @beginTime as the BeginTime
+    INSERT INTO OEE.EquipmentEvents (
+        EquipmentId,
         StateEventId,
         JobEventId,
         ShiftEventId,
         PerformanceEventId,
+        BeginTime
+    )
+    VALUES (
+        @equipmentId,
+        @stateEventId,
+        @jobEventId,
+        @shiftEventId,
+        @performanceEventId,
         @beginTime
-	FROM EquipmentEvents
-	WHERE EquipmentId = @equipmentId
-    AND Id = @lastEventId
+    )
 
+    -- Get the ID of the newly inserted record and assign it to @eventId
     SET @eventId = SCOPE_IDENTITY()
 END
 go
--- test/test.sql
+++ test/test.sql
@@ -1,12 +1,12 @@
 -- Test usp_BeginStateEvent
 DECLARE @beginTime datetime = '2/1/2024 00:02:06.000'--SYSUTCDATETIME()
-DECLARE @equipmentId int = OEE.fn_FindEquipmentByPath ('Site/Area/Line 1')
+DECLARE @equipmentId int = OEE.fn_FindEquipmentByPath ('Enterprise/Site/Area/Line 1')
 DECLARE @stateId int = OEE.fn_FindEquipmentStateByValue (@equipmentId, 1)
 EXECUTE OEE.usp_BeginStateEvent @equipmentId, @stateId, @beginTime
 
 -- Test usp_BeginJobEvent
 --DECLARE @beginTime datetime = SYSUTCDATETIME()
---DECLARE @equipmentId int = OEE.fn_FindEquipmentByPath ('Site/Area/Line 2')
+--DECLARE @equipmentId int = OEE.fn_FindEquipmentByPath ('Enterprise/Site/Area/Line 2')
 DECLARE @jobId int, @jobEventId int
 EXECUTE OEE.usp_FindOrCreateJobByReference '1000004', @jobId OUTPUT
 EXECUTE OEE.usp_BeginJobEvent @equipmentId, @jobId, @beginTime
