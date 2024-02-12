00243de0e4256e707e59f434107c4a3e5d637c6b
Tony Upton <13627984+tonyupton@users.noreply.github.com>
2024-02-11 22:53:07 -0500

    Update EquipmentEvents table and modify test.sql
    Expanded the EquipmentEvents table to include additional, performance-related fields. These are present to enhance data detailing and extraction capabilities. Also, minor modifications to the test.sql, such as changing specific return values and shift names. These changes serve to further test and ensure the dependability of our database scripts.

--- test/test.sql
+++ test/test.sql
@@ -1,7 +1,7 @@
 -- Test usp_BeginStateEvent
 DECLARE @beginTime datetime = SYSUTCDATETIME()
 DECLARE @equipmentId int = OEE.fn_FindEquipmentByPath ('Enterprise/Site/Area/Line 1')
-DECLARE @stateId int = OEE.fn_FindEquipmentStateByValue (@equipmentId, 0)
+DECLARE @stateId int = OEE.fn_FindEquipmentStateByValue (@equipmentId, 1)
 EXECUTE OEE.usp_BeginStateEvent @equipmentId, @stateId, @beginTime
 
 -- Test usp_BeginJobEvent
@@ -14,11 +14,11 @@ SELECT * FROM OEE.Jobs
 SELECT * FROM OEE.JobEvents
 
 -- Test usp_BeginShiftEvent
---DECLARE @beginTime datetime = SYSUTCDATETIME()
+DECLARE @beginTime datetime = SYSUTCDATETIME()
 DECLARE @shiftScheduleId int = OEE.fn_FindShiftScheduleByName ('Operations')
-DECLARE @shiftId int = OEE.fn_FindShiftByName (@shiftScheduleId, 'A')
+DECLARE @shiftId int = OEE.fn_FindShiftByName (@shiftScheduleId, 'C')
 EXECUTE OEE.usp_BeginShiftEvent @shiftScheduleId, @shiftId, @beginTime
---SELECT * FROM OEE.ShiftEvents
+SELECT * FROM OEE.ShiftEvents
 
 -- Select all from EquipmentEvents, calculate Minutes
 SELECT *, CONVERT(float, DATEDIFF(ms, BeginTime, ISNULL(EndTime, SYSUTCDATETIME()))) / 60000 Minutes
