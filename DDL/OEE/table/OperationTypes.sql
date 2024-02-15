9988260396553a81d30dfa7c00c85679eaa02271
Tony Upton <13627984+tonyupton@users.noreply.github.com>
2024-02-14 22:20:06 -0500

    Update OperationTypes table schema
    The "Scheduled" column name in the OperationTypes table has been renamed to "ExcludeFromOEE". This change was performed to better reflect the functionality of this column in the OEE calculation process.

--- DDL/OEE/table/OperationTypes.sql
+++ DDL/OEE/table/OperationTypes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE OEE.OperationTypes (
 	Id int IDENTITY,
-	Name varchar(50) NOT NULL,
-	Scheduled bit    NOT NULL,
+	Name varchar(50)   NOT NULL,
+	ExcludeFromOEE bit NOT NULL,
 	CONSTRAINT OperationTypes_pk PRIMARY KEY (Id),
 	CONSTRAINT OperationTypes_pk_2 UNIQUE (Name)
 )
